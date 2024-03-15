a3ea3863599e708cdcc466aa4f9537c35f088030
KalpeshBhoir <kalpeshbhoir2000@gmail.com>
2024-03-16 00:01:45 +0530

    header

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import Section from "./Section"
+
+const Hero = () => {
+  return (
+    <Section 
+    className="pt-[12rem] -mt-[5.25]"
+    crosses
+    crossesOffset
+    customPaddings
+    id="hero"
+    >
+        Hero
+    </Section>
+  )
+}
+
+export default Hero
